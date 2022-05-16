6482320c7fed819112fd59b88e2d3798cde2260c
yebin Park <yebinp1102@naver.com>
2022-05-16 15:14:50 +0900

    create format for error handling

--- api/index.js
+++ api/index.js
@@ -29,6 +29,16 @@ app.use("/api/users", usersRoute)
 app.use("/api/hotels", hotelsRoute)
 app.use("/api/rooms", roomsRoute)
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "문제가 발생 했습니다."
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack
+  })
+})
 
 app.listen(5000, () => {
   connect()
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import Hotel from '../models/Hotel.js'
+import { createError } from '../utils/error.js';
 
 const router = express.Router();
 
@@ -50,12 +51,12 @@ router.get('/:id', async (req, res)=>{
 })
 
 // 모든 호텔 데이터 가져오기
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res, next)=>{
   try{
     const hotels = await Hotel.find()
     res.status(200).json(hotels)
   }catch(err){
-    res.status(500).json(err)
+    next(err)
   }
 })
 
