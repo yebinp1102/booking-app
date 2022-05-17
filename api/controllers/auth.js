5b970642bccb4cdae4ccc9d99e66261f5f2649f7
yebin Park <yebinp1102@naver.com>
2022-05-17 14:24:06 +0900

    create jwt and cookie parser

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import AppUser from "../models/User.js"
 import bcrypt from 'bcrypt'
 import { createError } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res, next) => {
   try{
@@ -29,11 +30,14 @@ export const login = async (req, res, next) => {
     const isPasswordCorrect = await bcrypt.compareSync(req.body.password, user.password)
     if(!isPasswordCorrect) return next(createError(400, "비밀번호가 일치하지 않습니다."))
 
+    // 토큰 생성
+    const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_PRIVATE_KEY)
+
     // user를 디스트럭쳐링함으로써 다음 password와 isAdmin은 클라이언트에게 넘겨주지 않는다.
     const {password, isAdmin, ...otherDetails} = user._doc
 
-    // 유저명과 비밀번호 모두 일치
-    res.status(200).json({...otherDetails});
+    // 유저명과 비밀번호 모두 일치할 경우, cookie에 생성한 token을 저장하고 보안상의 이유로 http 접근만 허용
+    res.cookie("access_token", token, {httpOnly: true}).status(200).json({...otherDetails});
   }catch(err){
     next(err)
   }
