fd6808cbbea4a111a53db5919ec577f7983956f8
yebin Park <yebinp1102@naver.com>
2022-05-20 18:04:35 +0900

    create Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div>
-      React
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
