bbdb099ed40e4acec6cb49f3bd252ce77119a09b
yebin Park <yebinp1102@naver.com>
2022-05-21 22:06:08 +0900

    create featured component

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from '../components/Header'
+import styled from 'styled-components'
+import Featured from '../components/Featured'
+
+const Home = () => {
+  return (
+    <HomePage>
+      <Header />
+      <div className='homeContainer'>
+        <Featured />
+        <h1 className='homeTitle'>유형별 숙소</h1>
+        <h1 className='homeTitle'>후기 좋은 숙소들</h1>
+      </div>
+    </HomePage>
+  )
+}
+
+export default Home
+
+const HomePage = styled.div`
+  .homeContainer{
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+  }
+
+  .homeTitle{
+    width: 1024px;
+    font-size: 20px;
+  }
+`;
